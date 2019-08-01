af9c53bb20b666a304f8b0124ba21f41256a1d0c
taka <fermatslasttheoremisdifficult@gmail.com>
2019-08-01 16:10:06 +0900

    write code but very slow

--- Problem650.py
+++ Problem650.py
@@ -10,73 +10,123 @@
 # σ(p^e) = (p^(e+1) - 1) / (p - 1)
 # D(n) = σ(B(n)) = σ(B(n-1)) * σ(n^n / n!)
 #
-# n = Πp[i]^e[i] と書き, factor(n) := {p[0]:e[0], p[1]:e[1], ..., p[k]:e[k]}
+# n = Πp[i]^e[i] と書き, factor(n) := [e[0], e[1], ..., e[k]
 # と定義し,
-# factor(n) + factor(m) は辞書のキーの和またはキーの追加,
-# factor(n) - factor(m) は辞書のキーの差またはキーの削除,
-# factor(n)^a = {p[0]:a*e[0], p[1]:a*e[1], ..., p[k]:a*e[k]} で定める.
+# factor(n) + factor(m) はリストのそれぞれの要素の和
+# factor(n) - factor(m) はリストのそれぞれの要素の差
+# factor(n)^a = [a*e[0], a*e[1], ..., a*e[k]] で定める.
 #
 # n * m = factor(n) + factor(m), n / m = facotr(n) - factor(m) である.
-# factor(n!) = factor((n-1)!) + factor(n)
 # factor(B(n)) = factor(B(n-1)) + factor(n)^n - factor(n!)
+# n! の p の指数は n//p + n//p^2 + n//p^3 + n//p^4 + ... である.
 
-from functools import lru_cache, reduce
+from functools import reduce
 
 
-@lru_cache(maxsize=256)
+def memo(f):
+    check = {}
+
+    def func(*args):
+        if args not in check:
+            check[args] = f(*args)
+        return check[args]
+
+    return func
+
+
+def eratosthenes(n):
+
+    primes = [False if i % 2 == 0 else True for i in range(n + 1)]
+    primes[1], primes[2] = False, True
+
+    for i in range(3, int(n**0.5) + 1, 2):
+        if primes[i]:
+            for j in range(2 * i, n + 1, i):
+                primes[j] = False
+    return primes
+
+
+@memo
 def B(n):
+
     if n == 1:
-        return 1
+        return [0] * len(primes)
     else:
-        return B(n - 1) * n**n // reduce(lambda x, y: x * y, range(1, n + 1))
+        nn = [n * i for i in val2list(n)]
+        fact_n = factorial(n)
+        return es_sub(es_add(B(n - 1), nn), fact_n)
+
+
+@memo
+def inverse(n, m=10**9 + 7):
+    return pow(n, m - 2, m)
 
 
 def D(n):
-    pass
-
-
-# n! を素因数分解した形で返す
-@lru_cache(maxsize=256)
-def factorial(n, e_dict={}):
     if n == 1:
-        return e_dict
+        return 1
     else:
-        return factorial(n - 1, dict_add(e_dict, val2dict(n)))
+        es = B(n)
+        sum_d = 1
+        for p, e in zip(primes, es):
+            sum_d = sum_d * (pow(p, e + 1) - 1) // (p - 1) % m
+        return sum_d
 
 
-def val2dict(n):
-    pass
+@memo
+def factorial(n):
+    if n == 1 or n == 0:
+        return [0] * len(primes)
+    else:
+        return es_add(factorial(n - 1), val2list(n))
+
+    # es = [0] * len(primes)
+    # for i, p in enumerate(primes):
+    #     e = 0
+    #     tmp_p = p
+    #     if n < p:
+    #         break
+    #     while n >= p:
+    #         e += n // p
+    #         p *= tmp_p
+    #     es[i] = e
+    # return es
 
 
-def dict2val(es, m=1000000007):
+def val2list(n):
+    es = [0] * len(primes)
+    for i, p in enumerate(primes):
+        cnt_p = 0
+        while n % p == 0:
+            cnt_p += 1
+            n //= p
+        es[i] = cnt_p
+        if n == 1:
+            break
+    return es
+
+
+def list2val(es, m=1000000007):
     return reduce(lambda x, y: x * y % m,
-                  [pow(p, e, m) for p, e in es.items()])
+                  [pow(p, e, m) for p, e in zip(primes, es)])
 
 
-def dict_add(d1, d2):
-    d = d1.copy()
-    for k, v in d2.items():
-        if k in d:
-            d[k] += v
-        else:
-            d[k] = v
-    return d
+def es_add(e1, e2):
+    return [a + b for a, b in zip(e1, e2)]
 
 
-def dict_sub(d1, d2):
-    d = d1.copy()
-    for k, v in d2.items():
-        if k in d:
-            d[k] -= v
-            if d[k] == 0:
-                del d[k]
-        else:
-            d[k] = -v
-    return d
+def es_sub(e1, e2):
+    return [a - b for a, b in zip(e1, e2)]
+
+
+N = 20000
+m = 10**9 + 7
+primes_bool = eratosthenes(N)
+primes = [i for i, p in enumerate(primes_bool) if p]
 
 
 def main():
-    pass
+    return sum(D(i) for i in range(1, N + 1)) % m
 
 
 if __name__ == "__main__":
