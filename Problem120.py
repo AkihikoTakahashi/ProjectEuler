6d0bd78840978baea56ef6fc6b9bd77ec8d81422
taka <fermatslasttheoremisdifficult@gmail.com>
2019-09-07 08:54:04 +0900

    update code, comment

--- Problem120.py
+++ Problem120.py
@@ -1,21 +1,54 @@
 # coding: utf-8
 
+# f(n) = (a - 1)^n + (a + 1)^n とおく.
+#
 # (a-1)^n = (-1)^n + n * a * (-1)^(n-1) mod a^2
 #           = 1 - n * a (n: 偶数)
 #             n * a - 1 (n: 奇数)
 # (a+1)^n = 1 + n * a
-# よって (a - 1) ^ n + (a + 1) ^ n
-#        = 2 (n: 偶数)
-#          2 * n * a mod a^2 (n: 奇数)
+# よって
+# f(n) = 2                 (n: 偶数)
+#        2 * n * a mod a^2 (n: 奇数)
 # n = 1 のとき 2 * a mod a^2 > 2 より
-# r_max = max(2 * n * a mod a^2)
+# r_max = max(2 * n * a mod a^2) と書ける.
 #
-# 考える n は n = a のとき 2 * n * a = 0 mod a^2 なので,
-# 0 <= n <= a - 1
+# (1) a = 0 mod 4 のとき
+# n = (a - 2)/ 2 のとき f(n) は最大値となる.
+# f(n) = a * (a - 2) mod a^2 となるが,
+# f(n) はすべて 2 * a の倍数であり, a * (a - 2) は
+# a^2 未満のなかで最大の 2 * a の倍数だからである.
+#
+# (2) a = 1 mod 4 のとき
+# n = (a - 3) / 2 のとき f(n) は最大値となる.
+# f(n) = a * (a - 3) mod a^2 となり,
+# a^2 未満のなかで最大の 2 * a の倍数である.
+#
+# 残りも同様に,
+#
+# (3) a = 2 mod 4 のとき
+# n = a - 1 のとき f(n) は最大値 a * (a - 2) を取る.
+#
+# (4) a = 3 mod 4 のとき
+# n = (a - 1) / 2 のとき f(n) は最大値 a * (a - 1) を取る.
+#
+# (1) から (4) を総括すると, f(n) の最大値 r_max は
+# a = 0 mod 2 のとき
+#   r_max = a * (a - 2)
+# a = 1 mod 4 のとき
+#   r_max = a * (a - 3)
+# a = 3 mod 4 のとき
+#   r_max = a * (a - 1)
+# が言える.
 
 
 def calc(a):
-    return max(2 * n * a % a**2 for n in range(a))
+    if a % 2 == 0:
+        r_max = a * (a - 2)
+    elif a % 4 == 1:
+        r_max = a * (a - 3)
+    else:
+        r_max = a * (a - 1)
+    return r_max
 
 
 def main():
