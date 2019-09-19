1db21486739ce8cd4f9cf8d122eb70d762bff6a9
taka <fermatslasttheoremisdifficult@gmail.com>
2019-09-19 12:19:25 +0900

    update

--- Problem655.py
+++ Problem655.py
@@ -1,32 +1,41 @@
 # coding: utf-8
 
+# X を 32 桁の数とする.
+# X = ABD は回文数かつ N の倍数とする
+# B は 16桁の回文数
 
-def gen_palindrom(palindroms, stop_digit=32):
-    def digit(n):
-        d = 0
-        while n != 0:
-            d += 1
-            n //= 10
-        return d
 
-    while True:
-        new_palindroms = []
-        for palindrom in palindroms:
+# 6桁の回文数は 4桁の回文数を10倍し, 100001(=10**6+1)の倍数を足す
+def gen_palindromic(digit, l=0):
+    '''digit桁の回文数を生成する'''
 
-            d = digit(palindrom)
-            for i in range(1, 10):
-                new_palindrom = i * (10**(d + 1) + 1) + 10 * palindrom
-                yield new_palindrom
-                new_palindroms.append(new_palindrom)
-
-        if d + 2 > stop_digit:
-            break
-
-        palindroms = new_palindroms
+    if digit == l:
+        yield 0
+    elif digit - 1 == l:
+        for i in range(10):
+            yield i
+    else:
+        start_i = 1 if l == 0 else 0
+        multi = 10**(digit - l - 1) + 1
+        for i in range(start_i, 10):
+            for palindromic in gen_palindromic(digit, l + 2):
+                yield i * multi + 10 * palindromic
 
 
 def main():
-    for i in gen_palindrom(
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99]):
-        if i % 109 == 0:
-            print(i)
+    # def is_palindromic(n):
+    #     return str(n) == str(n)[::-1]
+
+    # # N = 10000019
+    # # L = 10**32
+    N = 109
+    L = 10**5
+
+    return sum(pa % N == 0 for i in range(3, 6) for pa in gen_palindromic(i))
+
+
+if __name__ == '__main__':
+    # N = 10000019
+    # a = set(i * pow(10, 8, N) % N for i in gen_palindromic(16))
+    # print(len(a))
+    print(main())
