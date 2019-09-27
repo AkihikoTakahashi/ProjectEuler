d950b97cce84c90e6a11a8441535de5b05029f40
taka <fermatslasttheoremisdifficult@gmail.com>
2019-09-27 11:10:41 +0900

    write code but incorrect

--- Problem655.py
+++ Problem655.py
@@ -1,11 +1,6 @@
 # coding: utf-8
 
-# X を 32 桁の数とする.
-# X = ABD は回文数かつ N の倍数とする
-# B は 16桁の回文数
 
-
-# 6桁の回文数は 4桁の回文数を10倍し, 100001(=10**6+1)の倍数を足す
 def gen_palindromic(digit, l=0):
     '''digit桁の回文数を生成する'''
 
@@ -15,27 +10,56 @@ def gen_palindromic(digit, l=0):
         for i in range(10):
             yield i
     else:
-        start_i = 1 if l == 0 else 0
+        # start_i = 1 if l == 0 else 0
+        start_i = 0
         multi = 10**(digit - l - 1) + 1
         for i in range(start_i, 10):
             for palindromic in gen_palindromic(digit, l + 2):
                 yield i * multi + 10 * palindromic
 
 
+def rev(n):
+    rev_n = 0
+    while n > 0:
+        rev_n = rev_n * 10 + n % 10
+        n //= 10
+    return rev_n
+
+
+# 4 の倍数 + 1 ... 123454321 (9) -> 123*10^6 + 454*10^3 + 321
+# 4 の倍数 + 2 ... 1234554321 (10) -> 123*10^7 + 4554*10^3 + 321
+# 4 の倍数 + 3 ... 12345654321 (11) -> 123*10^8 + 45654*10^3 + 321
+# 4 の倍数 ....... 123456654321 (12) -> 123*10^9 + 456654*10^3 + 321
+def count_ans(digits, N=10000019):
+    from math import ceil
+    from collections import Counter
+
+    d = ceil(digits / 4)
+
+    pal_d = digits - 2 * d
+    inners = Counter(i * pow(10, d, N) % N for i in gen_palindromic(pal_d))
+    outers = Counter((i * pow(10, pal_d + d, N) + rev(i)) % N
+                     for i in range(10**(d - 1), 10**d))
+
+    return sum(outers[N - k] * v for k, v in inners.items())
+
+
 def main():
-    # def is_palindromic(n):
-    #     return str(n) == str(n)[::-1]
+    N = 10000019
+    L = 32
 
-    # # N = 10000019
-    # # L = 10**32
-    N = 109
-    L = 10**5
-
-    return sum(pa % N == 0 for i in range(3, 6) for pa in gen_palindromic(i))
+    cnt = 0
+    for i in range(8, L + 1):
+        _cnt = count_ans(i, N)
+        cnt += _cnt
+        print(i, _cnt)
+    # return sum(count_ans(i, N) for i in range(8, L + 1))
+    return cnt
 
 
 if __name__ == '__main__':
     # N = 10000019
     # a = set(i * pow(10, 8, N) % N for i in gen_palindromic(16))
     # print(len(a))
+
     print(main())
