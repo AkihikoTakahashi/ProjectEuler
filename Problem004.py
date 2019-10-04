57baaab9358d3c431b769c339283c5314541125f
taka <fermatslasttheoremisdifficult@gmail.com>
2019-10-04 15:03:15 +0900

    change to use PriorityQueue

--- Problem004.py
+++ Problem004.py
@@ -1,17 +1,42 @@
 # coding: utf-8
 
+# 総当たりでも解けるが, PriorityQueue を使ってみる.
+
+from queue import PriorityQueue
+
 
 def is_palindromic(n):
     str_n = str(n)
     return str_n == str_n[::-1]
 
 
+def gen_sorted(init, nexts):
+    pq = PriorityQueue()
+    pq.put(init)
+
+    while not pq.empty():
+        z0 = pq.get()
+        yield z0
+        for z in nexts(*z0):
+            pq.put(z)
+
+
+def gen_product_descendent(max_n):
+    def nexts(p, x, y):
+        if x < 100 or y < 100:
+            raise StopIteration
+        if x > y:
+            yield -(x - 1) * y, x - 1, y
+        if x == max_n:
+            yield -x * (y - 1), x, y - 1
+
+    return gen_sorted((-max_n * max_n, max_n, max_n), nexts)
+
+
 def main():
-    return max([
-        a * b for a in range(100, 1000) for b in range(100, 1000)
-        if is_palindromic(a * b)
-    ])
+    return next(-i[0] for i in gen_product_descendent(999)
+                if is_palindromic(-i[0]))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print(main())
