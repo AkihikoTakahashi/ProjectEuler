a0e28ad18c69a49843ad8d9aa52f1a89039efea6
taka <fermatslasttheoremisdifficult@gmail.com>
2019-12-06 16:49:15 +0900

    first commit

---
+++ Problem055.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+
+def rev(n):
+    s = 0
+    while n > 0:
+        s = 10 * s + n % 10
+        n //= 10
+    return s
+
+
+def is_palindromic(n):
+    str_n = str(n)
+    return str_n[::-1] == str_n
+
+
+def is_lychrel(n, cnt=0):
+    if cnt == 50:
+        return True
+
+    next_n = n + rev(n)
+    if is_palindromic(next_n):
+        return False
+    else:
+        return is_lychrel(next_n, cnt + 1)
+
+
+def main():
+    return sum(1 for i in range(1, 10000) if is_lychrel(i))
+
+
+if __name__ == "__main__":
+    print(main())
