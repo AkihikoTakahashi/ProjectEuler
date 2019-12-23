5e99d0d07fe0711bbeb0e4aaa94288fdeec05857
taka <fermatslasttheoremisdifficult@gmail.com>
2019-12-23 16:32:47 +0900

    update code

--- Problem686.py
+++ Problem686.py
@@ -4,31 +4,34 @@
 #
 # N = 2^j  ==>  log10(N) = jlog10(2) より, k < jlog10(2) < k+1 とすると, N は k 桁である.
 # よって N = 10^k * 10^(jlog10(2) - k) である.
-# 10^k は先頭の数値に影響しないので, 10^(jlog10(2) - k) の先頭 3 文字が 123 であるか判定する.
+# 10^k は先頭の数値に影響しないので, 10^(jlog10(2) - k) の先頭が L か判定する.
 
 from math import log10
 from itertools import count, islice
 
 
 def gen_p(L):
-    len_L = int(log10(L)) + 1
+    d_L = int(log10(L)) + 1
+    log10_L = log10(L)
+    log10_L1 = log10(L + 1)
+
+    log10_2 = log10(2)
+    d = 0
     for j in count(1):
-        d = log10(2) * j
+        d += log10_2
         int_d = int(d)
 
-        if L < 10**(d - int_d) * 10**(len_L - 1) < L + 1:
+        # L < 10**(d - int_d + d_L - 1) < L + 1 ==>
+        # log10(L) < d - int_d + d_L - 1 < log10(L + 1)
+        if log10_L < d - int_d + d_L - 1 < log10_L1:
             yield j
-
-
-def nth(iterable, n):
-    return next(islice(iterable, n, None))
 
 
 def main():
     L = 123
     n = 678910
 
-    return nth(gen_p(L), n - 1)
+    return next(islice(gen_p(L), n - 1, None))
 
 
 if __name__ == '__main__':
