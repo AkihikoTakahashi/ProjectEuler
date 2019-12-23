ec23d8a4e9e3d216e491ab4fb6f7579533f3bd6f
taka <fermatslasttheoremisdifficult@gmail.com>
2019-12-23 12:22:59 +0900

    update code

--- Problem001.py
+++ Problem001.py
@@ -1,14 +1,18 @@
 # coding: utf-8
 
+# 3 の倍数または 5 の倍数の和は 3 の倍数の和 + 5 の倍数の和 - 15 の倍数の和
+
+
+def sum_n_multiple(n, L):
+    k = L // n
+    return (n + (n * k)) * k // 2
+
 
 def main():
 
     lim = 1000
-    s = 0
-    for i in range(lim):
-        if i % 3 == 0 or i % 5 == 0:
-            s += i
-    return s
+    return sum_n_multiple(3, lim - 1) + sum_n_multiple(
+        5, lim - 1) - sum_n_multiple(15, lim - 1)
 
 
 if __name__ == '__main__':
