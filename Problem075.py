175d5add17ff77e9ec52a6144b1054298a226cd1
taka <fermatslasttheoremisdifficult@gmail.com>
2020-01-07 12:49:33 +0900

    first commit

---
+++ Problem075.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+from math import gcd
+
+
+def gen_pythagoras(x):
+
+    for m in range(1, int((x // 2)**0.5) + 1):
+        for n in range(1, m):
+            if gcd(m, n) != 1 or m % 2 == n % 2:
+                continue
+
+            a = m**2 - n**2
+            b = 2 * m * n
+            c = m**2 + n**2
+
+            if a > b:
+                a, b = b, a
+
+            s = a + b + c
+            for i in range(1, x // s + 1):
+                yield a * i, b * i, c * i
+
+
+def main():
+    L = 1500000
+
+    wire_len = [0] * (L + 1)
+
+    pythagoras = gen_pythagoras(L)
+    for a, b, c in pythagoras:
+        wire_len[a + b + c] += 1
+
+    return sum(map(lambda x: x == 1, wire_len))
