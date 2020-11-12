2d0989d7957f9e850af4925a5f65cb8c3945307d
AkihikoTakahashi <fermatslasttheoremisdifficult@gmail.com>
2020-11-13 08:16:04 +0900

    update eratosthenes

--- Problem010.py
+++ Problem010.py
@@ -6,9 +6,9 @@ def eratosthenes(n):
     primes = [False if i % 2 == 0 else True for i in range(n + 1)]
     primes[1], primes[2] = False, True
 
-    for i in range(int(n**0.5) + 1):
+    for i in range(3, int(n**0.5) + 1, 2):
         if primes[i]:
-            for j in range(2 * i, n + 1, i):
+            for j in range(i * i, n + 1, i):
                 primes[j] = False
 
     p = [i for i in range(n + 1) if primes[i]]
