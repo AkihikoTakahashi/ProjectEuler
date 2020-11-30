5d0ea66b21cf3cacb7cc4de84ceb98c6b9aa9a21
AkihikoTakahashi <fermatslasttheoremisdifficult@gmail.com>
2020-11-30 22:24:13 +0900

    update sum of divisors algorithm

--- Problem095.py
+++ Problem095.py
@@ -1,17 +1,29 @@
 # coding: utf-8
 
 
-def get_divisors(n):
-    from functools import reduce
-    divisors = list(
-        set(
-            reduce(list.__add__,
-                   [[i, n // i]
-                    for i in range(1,
-                                   int(n**0.5) + 1) if n % i == 0])))
-    del divisors[divisors.index(n)]
+def calc_sum_divisors(n, include_n=False):
+    def prime_pow_sum(n, p):
+        s = 1
+        while n % p == 0:
+            n //= p
+            s = s * p + 1
+        return s, n
 
-    return divisors
+    nums = [i for i in range(n)]
+    factors = [1] * n
+
+    for p in filter(lambda p: nums[p] == p, range(2, int(n**0.5) + 1)):
+        for j in range(p, n, p):
+            s, m = prime_pow_sum(nums[j], p)
+            nums[j] = m
+            factors[j] *= s
+
+    for i in range(2, n):
+        if nums[i] != 1:
+            factors[i] *= (nums[i] + 1)
+        if not include_n:
+            factors[i] -= i
+    return factors
 
 
 def main():
@@ -24,7 +36,7 @@ def main():
             return _count_chain(divisors[n], cnt + 1, log + [n])
 
     limit = 1000000
-    divisors = [0] + [sum(get_divisors(i)) for i in range(1, limit)]
+    divisors = calc_sum_divisors(limit + 1)
 
     chains = [_count_chain(i) for i in range(limit)]
     return chains.index(max(chains))
