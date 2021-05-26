9f74b77d0be178a858a8bdee01a68a49bfe354ca
AkihikoTakahashi <fermatslasttheoremisdifficult@gmail.com>
2021-05-26 19:06:13 +0900

    update algorithm

--- Problem078.py
+++ Problem078.py
@@ -1,27 +1,38 @@
 # coding: utf-8
 
-from functools import lru_cache
-from itertools import count
+# p(k) = p(k-1) + p(k-2) - p(k-5) - p(k-7) + p(k-12) + p(k-15) ...
+# 1, 2, 5, 7, ... は拡張五角数
+#
+# https://ja.wikipedia.org/wiki/%E5%88%86%E5%89%B2%E6%95%B0
+
+from itertools import count, takewhile, cycle
 
 
-def p(n):
-    @lru_cache(maxsize=1024)
-    def _p(k, n):
-        if k > n:
-            return 0
-        if k == n:
-            return 1
-        else:
-            return _p(k + 1, n) + _p(k, n - k)
+def gen_ex_pentagonal():
+    '''拡張五角数を生成する.'''
+    for k in count(1):
+        n = (3 * k - 1) * k // 2
+        yield n
 
-    return _p(1, n)
+        # f(k) = (3k^2 - k) / 2 ==>
+        # f(-k) = (3k^2 + k) / 2 ==>
+        #       = f(k) + k
+        n += k
+        yield n
 
 
 def main():
     N = 10**6
-    for i in count(1):
-        if p(i) % N == 0:
-            return i
+    partitions = [1]
+    signs = (1, 1, -1, -1)
+    for n in count(1):
+        p_n = 0
+        for sign, k in zip(cycle(signs),
+                           takewhile(lambda k: k <= n, gen_ex_pentagonal())):
+            p_n = (p_n + sign * partitions[n - k]) % N
+        if p_n == 0:
+            return n
+        partitions.append(p_n)
 
 
 if __name__ == '__main__':
