2e2455b97ebbd0778d0c69a3daba5e8b1d0d78f3
AkihikoTakahashi <fermatslasttheoremisdifficult@gmail.com>
2022-02-10 16:56:51 +0900

    use prime_factors function

--- Problem003.py
+++ Problem003.py
@@ -1,16 +1,28 @@
 # coding: utf-8
 
 
+def prime_factors(n):
+    factors = []
+
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+
+    f = 3
+    while f * f <= n:
+        while n % f == 0:
+            factors.append(f)
+            n //= f
+        f += 2
+
+    if n != 1:
+        factors.append(n)
+    return factors
+
+
 def main():
     n = 600851475143
-    p = 2
-
-    while n != 1:
-        while n % p == 0:
-            n //= p
-        p += 1
-
-    return p - 1
+    return prime_factors(n)[-1]
 
 
 if __name__ == "__main__":
