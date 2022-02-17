c72f261e8787184fd0d56f84a4759db2a2cda90d
AkihikoTakahashi <fermatslasttheoremisdifficult@gmail.com>
2022-02-17 14:01:04 +0900

    use gen_primes, remove ex_sieve

--- Problem007.py
+++ Problem007.py
@@ -1,37 +1,25 @@
 # coding: utf-8
 
-from itertools import count, takewhile, islice
+from itertools import count, islice
 
 
-def ex_sieve(L=1000):
-    a = [True] * L
-    for p in (i for i in range(2, L) if a[i]):
-        for k in range(2 * p, L, p):
-            a[k] = False
+def gen_primes():
+    yield 2
+    primes = [2]
 
-    primes = [p for p in range(2, L) if a[p]]
-
-    for p in primes:
-        yield p
-
-    for m in count(1):
-        start = m * L
-        end = start + L
-
-        a = [True] * L
-        for p in takewhile(lambda p: p * p < end, primes):
-            k0 = (start + p - 1) // p * p - start
-            for k in range(k0, L, p):
-                a[k] = False
-
-        for p in (start + k for k in range(L) if a[k]):
-            primes.append(p)
-            yield p
+    for n in count(3, step=2):
+        for p in primes:
+            if n % p == 0:
+                break
+            if p * p > n:
+                yield n
+                primes.append(n)
+                break
 
 
 def main():
     cnt = 10001
-    return next(islice(ex_sieve(), cnt - 1, None))
+    return next(islice(gen_primes(), cnt - 1, None))
 
 
 if __name__ == "__main__":
